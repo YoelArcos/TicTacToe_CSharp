d022751618abd9fcb2efe98ff631b55410fd9487
YoelArcos <Yoel.arcoslopez@hotmail.com>
2024-05-29 14:58:12 +0200

    Implemented method checkforwinner and the game logic. Open for some extras

--- TicTacToeForms/Form1.Designer.cs
+++ TicTacToeForms/Form1.Designer.cs
@@ -37,6 +37,13 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.textBox_PlayerWins = new System.Windows.Forms.TextBox();
+            this.textBox_PlayerTwoWins = new System.Windows.Forms.TextBox();
+            this.label_PlayerWins = new System.Windows.Forms.Label();
+            this.label_PlayerTwoWins = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_gamesPlayed = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -45,8 +52,8 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 124);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -54,8 +61,8 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(123, 124);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -63,8 +70,8 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(123, 124);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
             // 
             // button4
             // 
@@ -72,8 +79,8 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(123, 124);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
             // 
             // button5
             // 
@@ -81,8 +88,8 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(123, 124);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
             // 
             // button6
             // 
@@ -90,8 +97,8 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(123, 124);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
             // 
             // button7
             // 
@@ -99,8 +106,8 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(123, 124);
             this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
             // 
             // button8
             // 
@@ -108,8 +115,8 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(123, 124);
             this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
             // 
             // button9
             // 
@@ -117,8 +124,72 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(123, 124);
             this.button9.TabIndex = 8;
-            this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(12, 50);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(121, 42);
+            this.btn_Reset.TabIndex = 9;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // textBox_PlayerWins
+            // 
+            this.textBox_PlayerWins.Location = new System.Drawing.Point(12, 134);
+            this.textBox_PlayerWins.Name = "textBox_PlayerWins";
+            this.textBox_PlayerWins.Size = new System.Drawing.Size(138, 22);
+            this.textBox_PlayerWins.TabIndex = 10;
+            // 
+            // textBox_PlayerTwoWins
+            // 
+            this.textBox_PlayerTwoWins.Location = new System.Drawing.Point(12, 213);
+            this.textBox_PlayerTwoWins.Name = "textBox_PlayerTwoWins";
+            this.textBox_PlayerTwoWins.Size = new System.Drawing.Size(138, 22);
+            this.textBox_PlayerTwoWins.TabIndex = 11;
+            // 
+            // label_PlayerWins
+            // 
+            this.label_PlayerWins.AutoSize = true;
+            this.label_PlayerWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_PlayerWins.ForeColor = System.Drawing.SystemColors.Control;
+            this.label_PlayerWins.Location = new System.Drawing.Point(12, 106);
+            this.label_PlayerWins.Name = "label_PlayerWins";
+            this.label_PlayerWins.Size = new System.Drawing.Size(118, 16);
+            this.label_PlayerWins.TabIndex = 12;
+            this.label_PlayerWins.Text = "Player One wins";
+            // 
+            // label_PlayerTwoWins
+            // 
+            this.label_PlayerTwoWins.AutoSize = true;
+            this.label_PlayerTwoWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_PlayerTwoWins.ForeColor = System.Drawing.SystemColors.Control;
+            this.label_PlayerTwoWins.Location = new System.Drawing.Point(12, 194);
+            this.label_PlayerTwoWins.Name = "label_PlayerTwoWins";
+            this.label_PlayerTwoWins.Size = new System.Drawing.Size(119, 16);
+            this.label_PlayerTwoWins.TabIndex = 13;
+            this.label_PlayerTwoWins.Text = "Player Two wins";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 302);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 22);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label_gamesPlayed
+            // 
+            this.label_gamesPlayed.AutoSize = true;
+            this.label_gamesPlayed.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_gamesPlayed.ForeColor = System.Drawing.SystemColors.Control;
+            this.label_gamesPlayed.Location = new System.Drawing.Point(13, 270);
+            this.label_gamesPlayed.Name = "label_gamesPlayed";
+            this.label_gamesPlayed.Size = new System.Drawing.Size(108, 16);
+            this.label_gamesPlayed.TabIndex = 15;
+            this.label_gamesPlayed.Text = "Games played";
             // 
             // TicTacToc_MainScreen
             // 
@@ -126,6 +197,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_gamesPlayed);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_PlayerTwoWins);
+            this.Controls.Add(this.label_PlayerWins);
+            this.Controls.Add(this.textBox_PlayerTwoWins);
+            this.Controls.Add(this.textBox_PlayerWins);
+            this.Controls.Add(this.btn_Reset);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -139,7 +217,9 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "TicTacToc_MainScreen";
             this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.TicTacToc_MainScreen_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -154,6 +234,13 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.TextBox textBox_PlayerWins;
+        private System.Windows.Forms.TextBox textBox_PlayerTwoWins;
+        private System.Windows.Forms.Label label_PlayerWins;
+        private System.Windows.Forms.Label label_PlayerTwoWins;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_gamesPlayed;
     }
 }
 
--- TicTacToeForms/Form1.cs
+++ TicTacToeForms/Form1.cs
@@ -12,9 +12,102 @@ namespace TicTacToeForms
 {
     public partial class TicTacToc_MainScreen : Form
     {
+        bool playerTurn = true;
+        int gamesPlayed = 0;
         public TicTacToc_MainScreen()
         {
             InitializeComponent();
+        }
+      
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.Text == "" )
+            {
+                btn.Text = playerTurn ? "X" : "O";
+                playerTurn = !playerTurn;
+                
+            }
+            CheckForWinner();
+            label_gamesPlayed.Text = gamesPlayed++.ToString();
+
+            
+        }
+
+        private void CheckForWinner()
+        {
+
+            // Definiere die möglichen Gewinnkombinationen
+            int[,] winCombinations = new int[,]
+            {   //a //b //c
+                {0, 1, 2}, // Erste Zeile
+                {3, 4, 5}, // Zweite Zeile
+                {6, 7, 8}, // Dritte Zeile
+                {0, 3, 6}, // Erste Spalte
+                {1, 4, 7}, // Zweite Spalte
+                {2, 5, 8}, // Dritte Spalte
+                {0, 4, 8}, // Erste Diagonale
+                {2, 4, 6}  // Zweite Diagonale
+            };
+
+            for (int i = 0; i < 8; i++)
+            {
+                int a = winCombinations[i, 0];
+                int b = winCombinations[i, 1];
+                int c = winCombinations[i, 2];
+
+                
+
+                Button b1 = this.Controls["button" + (a + 1).ToString()] as Button;
+                Button b2 = this.Controls["button" + (b + 1).ToString()] as Button;
+                Button b3 = this.Controls["button" + (c + 1).ToString()] as Button;
+
+               
+
+
+                if (b1.Text == "" || b2.Text == "" || b3.Text == "") continue; // Überspringe, wenn einer der Buttons leer ist
+
+                if (b1.Text == b2.Text && b2.Text == b3.Text)
+                {
+                    // Ein Gewinner wurde gefunden
+                    MessageBox.Show("Der Gewinner ist " + b1.Text);
+                    disableButtons();
+                    break; // Beende die Schleife, nachdem ein Gewinner gefunden wurde
+                }
+            }
+        }
+
+        private void disableButtons()
+        {
+            // Durchlaufe alle Controls und deaktiviere alle Buttons
+            foreach (Control c in Controls)
+            {
+                Button b = c as Button;
+                if (b != null)
+                {
+                    b.Enabled = false; // Deaktiviere den Button
+                }
+            }
+        }
+
+        private void TicTacToc_MainScreen_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
